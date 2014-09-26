618c64c8e91313375e4693c7358a997c13b61bce
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2014-09-26 15:42:37 +0200

    Fix bug in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8-*-
 from setuptools import setup
 
+long_description = 'Please see https://github.com/Holzhaus/python-cmuclmtk for details.'
+if os.access('README.md', os.R_OK):
+      with open('README.md') as f:
+            long_description = f.read()
+
 setup(name='cmuclmtk',
-      version='0.1.3',
+      version='0.1.4',
       description='Wrapper library for accessing the language model tools for CMU Sphinx (CMUCLMTK).',
-      long_description=open('README.md').read(),
+      long_description=long_description,
       author='Jan Holthuis',
       author_email='holthuis.jan@googlemail.com',
       license='BSD',
